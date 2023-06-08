334da972b18219b3caa3bc4fe2d97387ac2011f8
mike-data-nexus <112513079+mike-data-nexus@users.noreply.github.com>
2023-06-08 09:57:10 -0400

    init

---
+++ src/nft-pool.ts
@@ -0,0 +1,13 @@
+import { LockPosition } from "./../generated/templates/NFTPool/NFTPool";
+
+export function handleLockPosition(event: LockPosition): void {
+  // let entity = new FeePercentOwnershipTransferred(
+  //   event.transaction.hash.concatI32(event.logIndex.toI32())
+  // )
+  // entity.prevOwner = event.params.prevOwner
+  // entity.newOwner = event.params.newOwner
+  // entity.blockNumber = event.block.number
+  // entity.blockTimestamp = event.block.timestamp
+  // entity.transactionHash = event.transaction.hash
+  // entity.save()
+}
