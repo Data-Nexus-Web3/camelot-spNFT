b2bac4069c905c5f8a447b8cd537bf76b65471b3
mike-data-nexus <112513079+mike-data-nexus@users.noreply.github.com>
2023-06-08 22:53:05 -0400

    Protocol Created

--- src/modules/entity-factory.ts
+++ src/modules/entity-factory.ts
@@ -1,1 +1,18 @@
+import { Address } from "@graphprotocol/graph-ts";
+import { Protocol } from "../../generated/schema";
+
 //get protocol etc
+export function getProtocol(): Protocol {
+  let protocol = Protocol.load(
+    Address.fromString("0x6EcCab422D763aC031210895C81787E87B43A652")
+  );
+
+  if (!protocol) {
+    protocol = new Protocol(
+      Address.fromString("0x6EcCab422D763aC031210895C81787E87B43A652")
+    );
+
+    protocol.poolCount = 0;
+  }
+  return protocol as Protocol;
+}
